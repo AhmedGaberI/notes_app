beabb71850f0bc9cd9bf3698298beb43c57739a0
AhmedGaberI <engahmedgaber4@htomail.com>
2023-10-11 12:52:56 +0300

    create fetch notes method

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
+const kNotesBox = 'notes_box';
 const kPrimaryColor = Color(0xff62FCD7);
---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() {
+    try {
+      var notesBox = Hive.box(kNotesBox);
+      List<dynamic> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes as List<NoteModel>));
+    } catch (e) {}
+  }
+}
